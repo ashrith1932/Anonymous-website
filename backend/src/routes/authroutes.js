1a8e19172fff2880576d90d1c549edf5a25926ff
Ashrith Reddy <tummalaashrithreddy2391@gmail.com>
2025-12-16 21:16:13 +0530

    route  update for username updation

--- backend/src/routes/authroutes.js
+++ backend/src/routes/authroutes.js
@@ -5,5 +5,7 @@ const  authcontrollers = require("../controllers/authcontrollers")
 router.post("/login",authcontrollers.login);
 router.post("/signup",authcontrollers.signup);
 router.get("/generateloginkey",authcontrollers.generateloginkey);
+router.post("/checkusername",authcontrollers.checkusername);
+router.post("/updatedata",authcontrollers.update);
 
 module.exports=router;
